3da1ac246ffebffb9ab9d0081c4a67f226cc02e4
yahyayabakan <yahyaayabakan007@gmail.com>
2022-06-08 13:07:17 +0100

    finally finished building api project

---
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,60 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return firstName;
+	}
+
+	public String getLastName() {
+		// TODO Auto-generated method stub
+		return lastName;
+	}
+
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	public void setFirstName(String firstName2) {
+		firstName= firstName2;
+		
+	}
+
+	public void setLastName(String lastName2) {
+		lastName=lastName2;
+		
+	}
+
+	public void setEmail(String email2) {
+		email=email2;
+		
+	}
+}
+
